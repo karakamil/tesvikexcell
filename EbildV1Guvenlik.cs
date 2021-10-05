a5013e3c263b520e59df96bc2c7760bc25622286
karakamil <90703440+karakamil@users.noreply.github.com>
2021-10-05 16:58:28 +0300

    054102021

--- EbildV1Guvenlik.cs
+++ EbildV1Guvenlik.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Net;
 using System.Text;
 using System.Windows.Forms;
@@ -34,58 +35,44 @@ namespace tesvik10
 
             var driverPath = Application.StartupPath;
             var chromeOptions = new ChromeOptions();
+            //11111111 -- CHROME BROWSER İN GİZLENMESİ İÇİN 
+            chromeOptions.AddArgument("headless");
+            chromeOptions.AddUserProfilePreference("profile.default_content_setting_values.automatic_downloads", 1);
+            //11111111
             chromeOptions.AddUserProfilePreference("intl.accept_languages", "tr");
             chromeOptions.AddUserProfilePreference("disable-popup-blocking", "true");
             var v1driver1 = new ChromeDriver(driverPath, chromeOptions);
             v1driver.v1driver1 = v1driver1;
 
-            //v1driver1.Navigate().GoToUrl("https://ebildirge.sgk.gov.tr/WPEB/amp/loginldap");
-
-            //v1driver1.Navigate().GoToUrl("https://ebildirge.sgk.gov.tr/WPEB/PG");
-
-
-
-            //pictureBox1.ImageLocation = v1driver1.FindElement(By.XPath("//*[@id=\"formA\"]/table/tbody/tr[5]/td/table/tbody/tr[2]/td[2]/img")).GetAttribute("");
-            //pictureBox1.LoadAsync(imageurl);
-            //var request = WebRequest.Create(imageurl);
-
-            //using (var response = request.GetResponse())
-            //using (var stream = response.GetResponseStream())
-            //{
-            //    pictureBox1.Image = Bitmap.FromStream(stream);
-            //}
-
-
-
-            //v1driver1.Navigate().GoToUrl("https://ebildirge.sgk.gov.tr/WPEB/PG");
-            // string Resim = v1driver1.FindElement(By.XPath("/html/body/img")).GetAttribute("src");
-
-            // WebClient wc = new WebClient();
-            // wc.DownloadFile(Resim,driverPath);
-
-            // pictureBox1.Image = Image.FromFile(Application.StartupPath + "\\PG.jpg");
-            // //Bitmap Resim = v1driver1.FindElement(By.XPath("//*[@id=\"formA\"]/table/tbody/tr[5]/td/table/tbody/tr[2]/td[2]/img")).GetAttribute("src");
-
-
-
-            //pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
-            ////pictureBox1.Image = Resim(v1driver1.FindElement(By.XPath("/html/body/img")).GetAttribute("src"));
-
-
-            //Bitmap Resim(string Url)
-
-            //{
-            //    WebRequest rs = WebRequest.Create(Url);
-            //    return (Bitmap)Bitmap.FromStream(rs.GetResponse().GetResponseStream());
-            //}
-
             v1driver1.Navigate().GoToUrl("https://ebildirge.sgk.gov.tr/WPEB/amp/loginldap");
-            IWebElement img = v1driver1.FindElement(By.XPath("//*[@id=\"formA\"]/table/tbody/tr[5]/td/table/tbody/tr[2]/td[2]/img"));
-            var url = img.GetAttribute("src");
-            WebClient downloader = new WebClient();
-            v1driver1.GetScreenshot().SaveAsFile("url");
-            downloader.DownloadFile(url.jpg, driverPath+"PG.JPG");
-            v1driver1.GetScreenshot(WebResponse)
+
+
+            var captchaImage = v1driver1.FindElement(By.XPath("//*[@id=\"formA\"]/table/tbody/tr[5]/td/table/tbody/tr[2]/td[2]/img"));
+
+            ITakesScreenshot ssdriver = v1driver1 as ITakesScreenshot;
+            Screenshot screenshot = ssdriver.GetScreenshot();
+
+            Point point = captchaImage.Location;
+            int width = captchaImage.Size.Width;
+            int height = captchaImage.Size.Height;
+
+            Rectangle section = new Rectangle(point, new Size(width, height));
+            Bitmap source = new Bitmap(new MemoryStream(screenshot.AsByteArray));
+
+            Bitmap finalCaptchImage = ImageAl(source, section);
+            pictureBox1.Image = finalCaptchImage;
+        }
+
+
+
+
+        private Bitmap ImageAl(Bitmap source, Rectangle section)
+        {
+            Bitmap bmp = new Bitmap(section.Width, section.Height);
+            Graphics g = Graphics.FromImage(bmp);
+            g.DrawImage(source, 0, 0, section, GraphicsUnit.Pixel);
+            return bmp;
         }
     }
 }
+
